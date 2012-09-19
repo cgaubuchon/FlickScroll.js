946deddd26fa8d987bf9d49220b17b0af2303873
Corey Aubuchon <corey@cgaubuchon.com>
2012-09-18 21:42:25 -0400

    Updated Event Handlers
    Updated event handlers on body element. Touchmove only works with
    preventDefault, causing the page to not scroll natively. This is a
    problem.

--- js/jquery.FlickScroll-0.1.js
+++ js/jquery.FlickScroll-0.1.js
@@ -5,9 +5,9 @@ FS.FlickScroll = (function(win, doc, $) {
 	'use strict';
 
 	var _name = "FlickScroll",
+		_documentEl = document.documentElement,
 		_defaults = {
-			$flickElement: $('body'),
-			$flickDistanceThreshold: 1000, //in pixels
+			$flickDistanceThreshold: 100, //in pixels
 			$flickTimeMax: 1000, //in milliseconds
 			$allowTapScroll: true, //allow scroll to positon on touch as well as a flick
 			axis: 'vertical', //Direction of scroll intent (horizontal, vertical, all) TODO: implement 'all'
@@ -22,7 +22,8 @@ FS.FlickScroll = (function(win, doc, $) {
 			'touchEndTime': 0,
 			'eventDirectionX': '',
 			'eventDirectionY': ''
-		};
+		},
+		_moveCount = 0;
 
 	var FlickScroll = function(options){
 		this.options = $.extend({}, _defaults, options);
@@ -37,17 +38,19 @@ FS.FlickScroll = (function(win, doc, $) {
 		_eventStorage.touchStartPositionX = event.touches[0].pageX;
 		_eventStorage.touchStartPositionY = event.touches[0].pageY;
 		_eventStorage.touchStartTime = Date.now();
+
+		_moveCount = 0;
+
 	}
 
 	function handleTouchMove(event){
+		event.preventDefault();
+
 		_eventStorage.touchEndPositionX = event.touches[0].pageX;
 		_eventStorage.touchEndPositionY = event.touches[0].pageY;
 		_eventStorage.touchEndTime = Date.now();
-	}
 
-	function handleTouchEnd(event){
-		//Set flick event end parameters
-		alert('touch end');
+		_moveCount = _moveCount+1;
 
 		//Calculate differences to detect direction and speed of flick event to test against settings.
 		var flickOptions = {
@@ -57,34 +60,34 @@ FS.FlickScroll = (function(win, doc, $) {
 		};
 
 		//Determine direction of flick x (left or right)
-		if(flickOptions.xDifference === 0 && FS.FlickScroll.options.$allowTapScroll === true){
-			_eventStorage.eventDirectionX = 'none';
-		}else{
-			if(flickOptions.xDifference > 0){
-				_eventStorage.eventDirectionX = 'left';
-			}else{
-				_eventStorage.eventDirectionX = 'right';
-			}
-		}
+		// if(flickOptions.xDifference === 0 && FS.FlickScroll.options.$allowTapScroll === true){
+		// 	_eventStorage.eventDirectionX = 'none';
+		// }else{
+		// 	if(flickOptions.xDifference > 0){
+		// 		_eventStorage.eventDirectionX = 'left';
+		// 	}else{
+		// 		_eventStorage.eventDirectionX = 'right';
+		// 	}
+		// }
 
-		//Determine direction of flick y (up or down)
-		if(flickOptions.yDifference === 0 && FS.FlickScroll.options.$allowTapScroll === true){
-			_eventStorage.eventDirectionY = 'none';
-		}else{
-			if(flickOptions.yDifference > 0){
-				_eventStorage.eventDirectionY = 'up';
-			}else{
-				_eventStorage.eventDirectionY = 'down';
-			}
-		}
+		// //Determine direction of flick y (up or down)
+		// if(flickOptions.yDifference === 0 && FS.FlickScroll.options.$allowTapScroll === true){
+		// 	_eventStorage.eventDirectionY = 'none';
+		// }else{
+		// 	if(flickOptions.yDifference > 0){
+		// 		_eventStorage.eventDirectionY = 'up';
+		// 	}else{
+		// 		_eventStorage.eventDirectionY = 'down';
+		// 	}
+		// }
 
 		//Check for time difference vs. set max time to define a flick vs. scroll
 			//Check for distance flicked vs. set distance threshold to define flick vs. scroll
-		if(flickOptions.totalFlickTime < FS.FlickScroll.options.$flickTimeMax){
-			triggerFlick(flickOptions);
-		}else{
-			console.log('Touch event took too long. Not treated as flick. Check $flickTimeMax option.');
-		}
+		// if(flickOptions.totalFlickTime < FS.FlickScroll.options.$flickTimeMax){
+		// 	triggerFlick(flickOptions);
+		// }else{
+		// 	console.log('Touch event took too long. Not treated as flick. Check $flickTimeMax option.');
+		// }
 
 		//Debug console to log values for testing only
 		if(FS.FlickScroll.options.debug === true){
@@ -92,8 +95,16 @@ FS.FlickScroll = (function(win, doc, $) {
 			$debugPanel.find('.time').html("Event Time: " + flickOptions.totalFlickTime/1000 + ' seconds');
 			$debugPanel.find('.posX').html("Pos X Difference: " + flickOptions.xDifference + "px");
 			$debugPanel.find('.posY').html("Pos Y Difference: " + flickOptions.yDifference + "px");
-			$debugPanel.find('.direction').html("Directions: " + _eventStorage.eventDirectionY + " " + _eventStorage.eventDirectionX);
+
+			$debugPanel.find('.count').html("Count: " + _moveCount);
+			
+			//$debugPanel.find('.direction').html("Directions: " + _eventStorage.eventDirectionY + " " + _eventStorage.eventDirectionX);
 		}
+	}
+
+	function handleTouchCancel(event){
+		//Set flick event end parameters
+		//alert('touch end');
 	}
 
 	function triggerFlick(options){
@@ -105,12 +116,10 @@ FS.FlickScroll = (function(win, doc, $) {
 	//--------------------------------------------------
 
 	function attachEvents(){
-		FS.FlickScroll.options.$flickElement.on({
-			"touchstart": handleTouchStart,
-			"touchend": handleTouchEnd,
-			"touchmove": handleTouchMove
-		});
-		
+
+		document.body.addEventListener('touchstart', handleTouchStart, false);
+		document.body.addEventListener('touchmove', handleTouchMove, false);
+
 	}
 
 	//--------------------------------------------------
@@ -121,7 +130,7 @@ FS.FlickScroll = (function(win, doc, $) {
 		attachEvents();
 
 		if(FS.FlickScroll.options.debug === true){
-			$('html').append('<div class="debug-panel"><div class="time"></div><div class="posX"></div><div class="posY"></div><div class="direction"></div></div>');
+			$('html').append('<div class="debug-panel"><div class="time"></div><div class="posX"></div><div class="posY"></div><div class="direction"></div><div class="count"></div></div>');
 		}
 
 		return this;
