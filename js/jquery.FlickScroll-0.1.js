1d0972f9823817961c25f1b2da0ca335fa7096ce
Corey Aubuchon <corey@cgaubuchon.com>
2012-09-20 15:04:33 -0400

    Beginning of requestAnimationFrame scroll
    Getting aquainted with rAF so playing around with scrolling the page to
    the intendd position. Touch events are complete for now. Will have
    tolook into a work around for Chrome on Android maybe at some point.

--- js/jquery.FlickScroll-0.1.js
+++ js/jquery.FlickScroll-0.1.js
@@ -7,11 +7,15 @@ FS.FlickScroll = (function(win, doc, $) {
 	var _name = "FlickScroll",
 		_documentEl = document.documentElement,
 		_defaults = {
-			$flickDistanceThreshold: 100, //in pixels
-			$flickTimeMax: 1000, //in milliseconds
-			$allowTapScroll: true, //allow scroll to positon on touch as well as a flick
+			flickDistanceThreshold: 200, //in pixels
+			flickTimeMax: 1000, //in milliseconds
+			allowTapScroll: false, //allow scroll to positon on touch as well as a flick
 			axis: 'vertical', //Direction of scroll intent (horizontal, vertical, all) TODO: implement 'all'
 			debug: true 
+		},
+		_scrollSpot = {
+			'x': 0,
+			'y': 0
 		},
 		_eventStorage = {
 			'touchStartPositionX': 0,
@@ -23,7 +27,8 @@ FS.FlickScroll = (function(win, doc, $) {
 			'eventDirectionX': '',
 			'eventDirectionY': ''
 		},
-		_moveCount = 0;
+		debugPanel = '',
+		_flickOptions = {};
 
 	var FlickScroll = function(options){
 		this.options = $.extend({}, _defaults, options);
@@ -35,80 +40,74 @@ FS.FlickScroll = (function(win, doc, $) {
 	
 	function handleTouchStart(event){
 		//Store first touch event data
-		_eventStorage.touchStartPositionX = event.touches[0].pageX;
-		_eventStorage.touchStartPositionY = event.touches[0].pageY;
+		_scrollSpot.x = event.touches[0].pageX;
+		_scrollSpot.y = event.touches[0].pageY;
+
+		_eventStorage.touchStartPositionX = event.touches[0].clientX;
+		_eventStorage.touchStartPositionY = event.touches[0].clientY;
 		_eventStorage.touchStartTime = Date.now();
 
-		_moveCount = 0;
+		debugPanel.find('.event').html('touchstart');
 
+		document.body.addEventListener('touchend', handleTouchEnd, false);
 	}
 
-	function handleTouchMove(event){
-		//event.preventDefault();
-
-		_eventStorage.touchEndPositionX = event.touches[0].pageX;
-		_eventStorage.touchEndPositionY = event.touches[0].pageY;
-		_eventStorage.touchEndTime = Date.now();
-
-		_moveCount = _moveCount+1;
-
-		//Calculate differences to detect direction and speed of flick event to test against settings.
-		var flickOptions = {
-			'totalFlickTime': _eventStorage.touchEndTime - _eventStorage.touchStartTime,
-			'xDifference': _eventStorage.touchStartPositionX - _eventStorage.touchEndPositionX,
-			'yDifference': _eventStorage.touchStartPositionY - _eventStorage.touchEndPositionY
-		};
-
-		//Determine direction of flick x (left or right)
-		if(flickOptions.xDifference === 0 && FS.FlickScroll.options.$allowTapScroll === true){
-			_eventStorage.eventDirectionX = 'none';
-		}else{
-			if(flickOptions.xDifference > 0){
-				_eventStorage.eventDirectionX = 'left';
-			}else{
-				_eventStorage.eventDirectionX = 'right';
-			}
-		}
-
-		// //Determine direction of flick y (up or down)
-		if(flickOptions.yDifference === 0 && FS.FlickScroll.options.$allowTapScroll === true){
-			_eventStorage.eventDirectionY = 'none';
-		}else{
-			if(flickOptions.yDifference > 0){
-				_eventStorage.eventDirectionY = 'up';
-			}else{
-				_eventStorage.eventDirectionY = 'down';
-			}
-		}
+	function handleTouchEnd(event){
+		debugPanel.find('.event').html('touchend');
 
 		//Check for time difference vs. set max time to define a flick vs. scroll
 			//Check for distance flicked vs. set distance threshold to define flick vs. scroll
-		// if(flickOptions.totalFlickTime < FS.FlickScroll.options.$flickTimeMax){
-		// 	triggerFlick(flickOptions);
-		// }else{
-		// 	console.log('Touch event took too long. Not treated as flick. Check $flickTimeMax option.');
-		// }
+		if(_flickOptions.totalFlickTime < FS.FlickScroll.options.flickTimeMax){
+			//TODO: Logic for if vertical or horizontal scroll is desired
+			if(_flickOptions.yDifference < FS.FlickScroll.options.flickDistanceThreshold && _flickOptions.yDifference > (FS.FlickScroll.options.flickDistanceThreshold*-1)){
+				triggerFlick();
+			}else{
+				debugPanel.find('.success').html("Touch distance too long");
+			}
+		}else{
+			console.log('Touch event took too long. Not treated as flick. Check $flickTimeMax option.');
+			debugPanel.find('.success').html("Touch time too long");
+		}
+		document.body.removeEventListener('touchend');
+	}
 
-		//Debug console to log values for testing only
-		if(FS.FlickScroll.options.debug === true){
-			var $debugPanel = $('html').find('.debug-panel');
-			$debugPanel.find('.time').html("Event Time: " + flickOptions.totalFlickTime/1000 + ' seconds');
-			$debugPanel.find('.posX').html("Pos X Difference: " + flickOptions.xDifference + "px");
-			$debugPanel.find('.posY').html("Pos Y Difference: " + flickOptions.yDifference + "px");
+	function handleTouchMove(event){
+		//Bug in Android Chrome only fires with preventDefault
+		//event.preventDefault();
 
-			$debugPanel.find('.count').html("Count: " + _moveCount);
-			
-			$debugPanel.find('.direction').html("Directions: " + _eventStorage.eventDirectionY + " " + _eventStorage.eventDirectionX);
+		//Only trigger with one finger flick
+		if(event.touches.length == 1){
+			_eventStorage.touchEndPositionX = event.touches[0].clientX;
+			_eventStorage.touchEndPositionY = event.touches[0].clientY;
+			_eventStorage.touchEndTime = Date.now();
+
+			//Calculate differences to detect direction and speed of flick event to test against settings.
+			_flickOptions = {
+				'totalFlickTime': _eventStorage.touchEndTime - _eventStorage.touchStartTime,
+				'xDifference': _eventStorage.touchStartPositionX - _eventStorage.touchEndPositionX,
+				'yDifference': _eventStorage.touchStartPositionY - _eventStorage.touchEndPositionY
+			};
+
+			//Debug console to log values for testing only
+			if(FS.FlickScroll.options.debug === true){
+				debugPanel.find('.event').html('touchmove');
+				debugPanel.find('.time').html("Time: " + _flickOptions.totalFlickTime + ' ms');
+				debugPanel.find('.posX').html("X: S:"+_eventStorage.touchStartPositionX + " E: " +_eventStorage.touchEndPositionX+ " D: " + _flickOptions.xDifference + "px");
+				debugPanel.find('.posY').html("Y: S:"+_eventStorage.touchStartPositionY + " E: " +_eventStorage.touchEndPositionY+ " D: " + _flickOptions.yDifference + "px");
+			}
 		}
 	}
 
 	function handleTouchCancel(event){
 		//Set flick event end parameters
 		//alert('touch end');
+		debugPanel.find('.event').html('touchcancel');
 	}
 
-	function triggerFlick(options){
-		console.log('triggerFlick', options);
+	function triggerFlick(){
+		updateScrollPosition(_scrollSpot.x, _scrollSpot.y);
+		debugPanel.find('.success').html("Successful Flick! "+_scrollSpot.x+', '+_scrollSpot.y);
+		$('html').append('<div style="position:absolute;top:'+_scrollSpot.y+'px;left:'+_scrollSpot.x+'px;height:1px;width:1px;background:red;"></div>');
 	}
 
 	//--------------------------------------------------
@@ -116,10 +115,30 @@ FS.FlickScroll = (function(win, doc, $) {
 	//--------------------------------------------------
 
 	function attachEvents(){
-
 		document.body.addEventListener('touchstart', handleTouchStart, false);
 		document.body.addEventListener('touchmove', handleTouchMove, false);
+	}
 
+	function updateScrollPosition(positionToX, positionToY){
+
+		var currentPositionY = window.scrollY,
+			currentPositionX = window.scrollX;
+		
+		if(currentPositionY < positionToY){
+			window.scrollTo(currentPositionX,currentPositionY+1);
+		}else{
+			window.scrollTo(currentPositionX,currentPositionY-1);
+		}
+
+		if(currentPositionY != positionToY || currentPositionX != positionToX){
+			requestAnimationFrame(updateScrollPosition(positionToX, positionToY));
+		}else{
+			return;
+		}
+
+		
+
+		
 	}
 
 	//--------------------------------------------------
@@ -130,7 +149,8 @@ FS.FlickScroll = (function(win, doc, $) {
 		attachEvents();
 
 		if(FS.FlickScroll.options.debug === true){
-			$('html').append('<div class="debug-panel"><div class="time"></div><div class="posX"></div><div class="posY"></div><div class="direction"></div><div class="count"></div></div>');
+			$('html').append('<div class="debug-panel"><div class="event"></div><div class="time"></div><div class="posX"></div><div class="posY"></div><div class="direction"></div><div class="success"></div></div>');
+			debugPanel = $('html').find('.debug-panel');
 		}
 
 		return this;
